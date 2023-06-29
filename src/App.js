e9c56aa74ecdcdc181d02fdec71d48de665d5493
Niyoj Oli <oliniyoj@gmail.com>
2023-06-29 16:26:53 +0000

    initializes the project

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Header from "./components/UI/Header/Header";
+
+const App = (props) => {
+	return (
+		<React.Fragment>
+			<Header />
+		</React.Fragment>
+	);
+};
+
+export default App;
