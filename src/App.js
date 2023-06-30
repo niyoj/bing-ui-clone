3a22508b542d758f10390defb4c61534dfaaec5c
Niyoj Oli <oliniyoj@gmail.com>
2023-06-30 01:06:41 +0000

    makes the chatbox wrapper ready

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import Header from "./components/UI/Header/Header";
 import SearchBar from "./components/SearchBar/SearchBar";
+import Chat from "./components/Chat/Chat";
 
 const App = () => {
 	return (
 		<React.Fragment>
 			<Header />
+			<Chat />
 			<SearchBar />
 		</React.Fragment>
 	);
---
+++ src/components/Chat/Chat.js
@@ -0,0 +1,9 @@
+import styles from "./Chat.module.css";
+
+const Chat = (props) => {
+	return (
+		<section className={styles["chat__wrapper"]}>THis is a chatbox</section>
+	);
+};
+
+export default Chat;
