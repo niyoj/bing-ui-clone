5e82c62155d8dd18611ceac3a55b9d9774d70df7
Niyoj Oli <oliniyoj@gmail.com>
2023-06-30 00:54:05 +0000

    integrates header and searchbar component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Header from "./components/UI/Header/Header";
+import SearchBar from "./components/SearchBar/SearchBar";
 
-const App = (props) => {
+const App = () => {
 	return (
 		<React.Fragment>
 			<Header />
+			<SearchBar />
 		</React.Fragment>
 	);
 };
